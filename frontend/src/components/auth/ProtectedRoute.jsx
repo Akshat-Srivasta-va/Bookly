4d124ed43938b11a4d8f5bad0b06a2dc8d82c0d2
Akshat Srivastava <srivastavaakshat52@gmail.com>
2025-12-01 01:01:15 +0530

    protect route setup completed

--- frontend/src/components/auth/ProtectedRoute.jsx
+++ frontend/src/components/auth/ProtectedRoute.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { useLocation } from 'react-router-dom';
 
-const ProtectedRoute = () => {
-  return (
-    <div>ProtectedRoute</div>
-  )
+const ProtectedRoute = ({children}) => {
+ const isAuthenticated = false; // Replace with actual authentication logic
+ const loading = false; // Replace with actual loading state
+ const location = useLocation
+
+ if(loading) {
+  // you can add a loading spinner here if you want
+  return <div>Loading...</div>;
+ }
+
+  if(!isAuthenticated) { 
+    return <Navigate to="/login" state={{from: location}} replace/>
+  }
+  return children;
 }
 
 export default ProtectedRoute
