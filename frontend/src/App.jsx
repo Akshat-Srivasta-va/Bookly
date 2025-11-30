7ca704ad2d8c6f278fca1158c3a5791740b0b5af
Akshat Srivastava <srivastavaakshat52@gmail.com>
2025-11-30 19:24:04 +0530

    dependencies installed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import ProtectedRoute from './components/auth/ProtectedRoute'
+import LandinPage from './pages/LandinPage'
+import SignupPage from './pages/SignupPage'
+
 
 const App = () => {
   return (
-    <div className='bg-yellow-500  text-3xl'>App</div>
+    <div>
+        <Routes>
+            {/* Protected Routes */}
+            <Route path="/" element={<LandinPage />}/>
+            <Route path="/login" element={<LoginPage />}/>
+            <Route path="/signup" element={<SignupPage />}/>
+        </Routes>
+    </div>
   )
 }
 
