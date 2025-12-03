ada948f8f443f7432c5176206a55f76e807df0a8
Akshat Srivastava <srivastavaakshat52@gmail.com>
2025-12-04 00:38:23 +0530

    all type usr setup

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -29,13 +29,13 @@ exports.registerUser = async (req, res) => {
     }
 
     // create user
-    const user = await User.create({name, email, password });
+    const user = await User.create({ name, email, password });
 
     if (user) {
-    res.status(201).json({
-     message: "User registered successfully",
-     token: generateToken(user._id),
-    })
+      res.status(201).json({
+        message: "User registered successfully",
+        token: generateToken(user._id),
+      });
     } else {
       res.status(400).json({ message: "Invalid user data" });
     }
@@ -51,9 +51,21 @@ exports.registerUser = async (req, res) => {
 exports.loginUser = async (req, res) => {
   const { email, password } = req.body;
   try {
+    const user = await User.findOne({ email }).select("+password");
 
-  } catch(error) {
-    res.status(500).json({ message: "Server error" });  
+    if (user && (await user.matchPassword(password))) {
+      res.json({
+        message: "Login successful",
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(401).json({ message: "Invalid email or password" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
   }
 };
 
@@ -63,8 +75,15 @@ exports.loginUser = async (req, res) => {
 
 exports.getProfile = async (req, res) => {
   try {
-
-  } catch(error) {
+    const user = await User.findById(req.user.id);
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      avtar: user.avtar,  
+      isPro: user.isPro,
+    })
+  } catch (error) {
     res.status(500).json({ message: "Server error" });
   }
 };
@@ -72,4 +91,22 @@ exports.getProfile = async (req, res) => {
 // @desc   Update user profile
 // @route  PUT /api/auth/me
 // @access Private
-exports.updateUserProfile = async (req, res) => {};
+exports.updateUserProfile = async (req, res) => {
+  try {
+const user = await User.findById(req.user.id);
+if(user) {
+  user.name = req.body.name || user.name;
+
+  const updatedUser = await user.save();
+
+  res.json({
+    _id: updatedUser._id,
+    name: updatedUser.name,
+  });
+} else {
+  res.json({ message: "User not found" });
+}
+  } catch(error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
